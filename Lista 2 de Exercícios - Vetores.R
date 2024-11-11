5d066a6c0faf9469c359862a7a49e865748c8661
Lucas-magalhaes97 <lucasmagalhaes@ufpr.br>
2024-11-11 17:28:56 -0300

    Parei na linha de alturas de 1,50 até 2,00

---
+++ Lista 2 de Exercícios - Vetores.R
@@ -0,0 +1,36 @@
+
+idade <- 25
+idade
+
+nome <- "maria"
+nome
+
+notas <- c(8,9,7,6)
+notas
+
+frutas <- c("maça", "banana", "laranja")
+frutas
+
+idades<- c(30,40,50)
+idades
+
+vetor_logico <- c("TRUE", "FALSE", "TRUE")
+vetor_logico
+
+vetor_1a10 <- c(1:10)
+vetor_1a10
+
+vetores_2a200 <- seq(from = 2,
+                     to = 200,
+                     by = 2)
+vetores_2a200
+
+rep5 <- rep(seq(from = 2,
+                to = 10,
+                by = 2), times = 5)
+rep5
+
+alturas <- seq(from = 1.50,
+             to = 2.00,
+             by = 0.50)
+alturas
